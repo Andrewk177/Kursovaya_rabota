742fafbdfce90027b5c554f57be37f12aa39c3eb
Andrey <a.k.177@mail.ru>
2024-02-22 09:46:38 +0300

    Скорректирован код, добавлен файл config

---
+++ main.py
@@ -0,0 +1,9 @@
+from config import FILE_PATH
+
+from src.utils import load_transactions, print_transactions, sort_transactions
+
+if __name__ == "__main":
+    raw_transactions = load_transactions(FILE_PATH)
+    sorted_by_date = sort_transactions(raw_transactions)
+
+    print_transactions(sorted_by_date)
--- src/main.py
+++
@@ -1,3 +0,0 @@
-#from utils import stars_in_number()
-#from utils import get_json_file()
-#from utils import date_format_sort_executed()
