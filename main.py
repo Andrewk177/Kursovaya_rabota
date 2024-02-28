804e96ad561e8b78b70f65f550911f307fe85918
Andrey <a.k.177@mail.ru>
2024-02-28 17:26:32 +0300

    Исправлен файл main, поправлен utils и перенесен файл operations

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from config import FILE_PATH
 
 from utils import load_transactions, print_transactions, sort_transactions
 
-if __name__ == "__main":
-    raw_transactions = load_transactions(FILE_PATH)
-    sorted_by_date = sort_transactions(raw_transactions)
+raw_transactions = load_transactions(FILE_PATH)
 
-    print_transactions(sorted_by_date)
+sorted_by_date = sort_transactions(raw_transactions)
+
+print_transactions(sorted_by_date)
